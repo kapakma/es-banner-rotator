ceff09eda16481b8ec6b61d7575da6dc54f96564
Allan Ma <allankma@gmail.com>
2023-04-09 19:07:31 -0400

    fix lint errors

--- js/BarTimer.js
+++ js/BarTimer.js
@@ -1,41 +1,36 @@
 import Timer from './Timer.js';
 
-//Bar Timer Class
-function BarTimer(context, options) {
-    if (this instanceof BarTimer) {
-        Timer.call(this, context, options);
-        this._$bar = $('<div/>');
-        this._$timer.addClass('br-bar-timer').addClass(/bottom/i.test(opts.position) ? 'br-bottom' : 'br-top').append(this._$bar);
-    }
-    else {
-        return new BarTimer(context, options);
-    }
+// Bar Timer Class
+function BarTimer (context, options) {
+  Timer.call(this, context, options);
+  this._$bar = $('<div/>');
+  this._$timer.addClass('br-bar-timer').addClass(/bottom/i.test(options.position) ? 'br-bottom' : 'br-top').append(this._$bar);
 }
 
 BarTimer.prototype = Object.create(Timer.prototype);
 BarTimer.prototype.constructor = BarTimer;
 
-BarTimer.prototype.start = function(delay) {
-    if (this._complete) {
-        this._delay = delay;
-    }
+BarTimer.prototype.start = function (delay) {
+  if (this._complete) {
+    this._delay = delay;
+  }
 
-    this._startTime = Date.now();
-    this._$bar.transition({width:'101%'}, delay, 'linear');
-    Timer.prototype.start.call(this);
+  this._startTime = Date.now();
+  this._$bar.transition({ width: '101%' }, delay, 'linear');
+  Timer.prototype.start.call(this);
 };
 
-BarTimer.prototype.stop = function() {
-    this._elapsed = 0;
-    this._$bar.stopTransition(true).width(0);
-    Timer.prototype.stop.call(this);
+BarTimer.prototype.stop = function () {
+  this._elapsed = 0;
+  this._$bar.stopTransition(true).width(0);
+  Timer.prototype.stop.call(this);
 };
 
-BarTimer.prototype.pause = function() {
-    this._$bar.stopTransition(true);
-    this._elapsed += Date.now() - this._startTime;
-    this._$bar.width(`${this._elapsed/this._delay * 101}%`);
-    Timer.prototype.pause.call(this);
+BarTimer.prototype.pause = function () {
+  this._$bar.stopTransition(true);
+  this._elapsed += Date.now() - this._startTime;
+  this._$bar.width(`${this._elapsed / this._delay * 101}%`);
+  Timer.prototype.pause.call(this);
 };
 
 export default BarTimer;
--- js/Timer.js
+++ js/Timer.js
@@ -1,54 +1,49 @@
-//Timer Class
-function Timer(context, options) {
-    if (this instanceof Timer) {
-        if (context) {
-            this._running = false;
-            this._complete = true;
-            this._$timer = $('<div/>').appendTo(context._$screen).addTransitionClass('br-element-transition');
-            
-            if ($.isFunction(options.click)) {
-                this._$timer.css({cursor:'pointer'}).on('click', options.click);
-            }
-
-            this.addOnHover(context._$outmost, context._namespace);
-        }
-    }
-    else {
-        return new Timer();
-    }
-}
-
-Timer.prototype.start = function() {
-    this._running = true;
-    this._complete = false;
-    this._$timer.addClass('br-on');
-    this.wake();
-};
-
-Timer.prototype.stop = function() {
+// Timer Class
+function Timer (context, options) {
+  if (context) {
     this._running = false;
     this._complete = true;
-    this._$timer.removeClass('br-on');
-};
+    this._$timer = $('<div/>').appendTo(context._$screen).addTransitionClass('br-element-transition');
 
-Timer.prototype.pause = function() {
-    this._running = false;
-    this.sleep();
-};
-
-Timer.prototype.wake = function() {
-    this._$timer.removeClass('br-timer-sleep');
-};
-
-Timer.prototype.sleep = function() {
-    if (!this._running) {
-        this._$timer.addClass('br-timer-sleep');
+    if ($.isFunction(options.click)) {
+      this._$timer.css({ cursor: 'pointer' }).on('click', options.click);
     }
+
+    this.addOnHover(context._$outmost, context._namespace);
+  }
+}
+
+Timer.prototype.start = function () {
+  this._running = true;
+  this._complete = false;
+  this._$timer.addClass('br-on');
+  this.wake();
 };
 
-Timer.prototype.addOnHover = function($parent, namespace) {
-    $parent.on(`mouseenter${namespace}`, () => this.wake())
-           .on(`mouseleave${namespace}`, () => this.sleep());
+Timer.prototype.stop = function () {
+  this._running = false;
+  this._complete = true;
+  this._$timer.removeClass('br-on');
+};
+
+Timer.prototype.pause = function () {
+  this._running = false;
+  this.sleep();
+};
+
+Timer.prototype.wake = function () {
+  this._$timer.removeClass('br-timer-sleep');
+};
+
+Timer.prototype.sleep = function () {
+  if (!this._running) {
+    this._$timer.addClass('br-timer-sleep');
+  }
+};
+
+Timer.prototype.addOnHover = function ($parent, namespace) {
+  $parent.on(`mouseenter${namespace}`, () => this.wake())
+    .on(`mouseleave${namespace}`, () => this.sleep());
 };
 
 export default Timer;
